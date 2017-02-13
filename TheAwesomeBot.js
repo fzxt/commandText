9afe67c259d6953f480f2a1851cee04be7af0c5d
Şamil NART <samil.nart@gmail.com>
2017-02-13 11:03:14 +0300

    Added uptime integration

--- TheAwesomeBot.js
+++ TheAwesomeBot.js
@@ -12,7 +12,6 @@ class TheAwesomeBot {
     this.settings.tokens = Tokens; // insert tokens into our settings obj
     this.commands = {};
     this.usageList = '';
-
     // store the RE as they're expensive to create
     this.cmd_re = new RegExp(`^${this.settings.bot_cmd}\\s+([^\\s]+)\\s*([^]*)\\s*`, 'i');
   }
@@ -91,6 +90,11 @@ class TheAwesomeBot {
 
   loadCommands(cmdList) {
     this.usageList = '';
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const uptimeScript = require(path.join(__dirname, 'lib', 'uptime', 'uptime.js'));
+    this.usageList += `\n- ${this.settings.bot_cmd} ${uptimeScript.usage}`;
+    this.commands.uptime = uptimeScript;
+
     cmdList.forEach((cmd) => {
       const fullpath = path.join(__dirname, 'commands', cmd, `${cmd}.js`);
       const script = require(fullpath); // eslint-disable-line global-require, import/no-dynamic-require
