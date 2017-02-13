6e0b2d13f691a5ea568b11f08ef21902a58b25ca
Şamil NART <samil.nart@gmail.com>
2017-02-13 20:59:40 +0300

    something

--- commands/info/info.js
+++ commands/info/info.js
@@ -7,43 +7,55 @@ const githubRepo = 'https://github.com/$repo';
 const commitTemplate = '$commit $message';
 let config;
 let uptime;
-let lastCommit;
-let currentCommit;
+let lastCommit = {};
+let currentCommit = {};
 let embed;
+const githubHeaders = {
+  'User-Agent': 'TheAwesomeBot',
+  'Accept': 'application/vnd.github.v3+json', // eslint-disable-line quote-props
+};
 
 function infoInit(bot) {
   config = bot.settings.info;
   uptime = bot.commands.uptime;
   // get current checked out commit from git
   exec('git show --oneline -s', (err, stdout) => {
-    currentCommit = stdout.replace('\n', '');
+    const gitOutput = stdout.replace('\n\n', '').replace('\n', '');
+    currentCommit.shortSHA = gitOutput.split(' ')[0];
+    currentCommit.message = gitOutput;
+    request({
+      url: githubAPI.replace('$repo', config.repo) + '/' +currentCommit.shortSHA,
+      headers: githubHeaders,
+    }, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        lastCommit.link = 'https://github.com/404';
+        return;
+      }
+      lastCommit.link = JSON.parse(body).html_url;
+    });
   });
   // get latest commit in git repo
   request({
     url: githubAPI.replace('$repo', config.repo),
-    headers: {
-      'User-Agent': 'TheAwesomeBot',
-      'Accept': 'application/vnd.github.v3+json', // eslint-disable-line quote-props
-    },
+    headers: githubHeaders,
   }, (err, response, body) => {
-    if (err) {
-      lastCommit = 'Couldn\'t retrieve commit data.';
+    if (err || response.statusCode !== 200) {
+      lastCommit.link = 'https://github.com/404';
+      lastCommit.message = 'Couldn\'t retrieve commit data.';
       return;
     }
     const commitData = JSON.parse(body);
-
+    // TODO (sam): Instead of depending on nullness of the variable,
+    // act according to github api docs
     if (commitData[0] == null) {
-      lastCommit = 'Couldn\'t retrieve commit data.';
+      lastCommit.message = 'Couldn\'t retrieve commit data.';
       return;
     }
     const commitMessage = commitData[0].commit.message.replace('\n\n', ' ')
       .replace('\n', ' ');
-    lastCommit = commitTemplate.replace('$commit', commitData[0].sha.slice(0, 7))
+    lastCommit.message = commitTemplate.replace('$commit', commitData[0].sha.slice(0, 7))
       .replace('$message', commitMessage);
-    
-    // if (lastCommit.length > 25) {
-    //   lastCommit = lastCommit.slice(0, 22).concat('...');
-    // }
+    lastCommit.link = commitData[0].html_url;
   });
 }
 
@@ -61,11 +73,11 @@ module.exports = {
     let up = uptime.getUptime();
     embed = new discord.RichEmbed();
     embed.setColor('#4286f4')
-         .setAuthor('TheAwesomeBot', bot.client.user.avatarURL, githubRepo.replace('$repo', config.repo))
-         .setFooter(description.replace('cmd', bot.settings.bot_cmd))
-         .addField('Uptime', uptime.getUptime())
-         .addField('Latest Commit', lastCommit)
-         .setDescription('An open source bot made with :heart:');
+      .setAuthor('TheAwesomeBot', bot.client.user.avatarURL, githubRepo.replace('$repo', config.repo))
+      .setFooter(description.replace('cmd', bot.settings.bot_cmd))
+      .addField('Uptime', uptime.getUptime())
+      .addField('Latest Commit', lastCommit)
+      .setDescription('An open source bot made with :heart:');
     // let the users know if bot is working on a rolled back commit
     if (currentCommit !== lastCommit) embed.addField('Current Commit', currentCommit);
     message.channel.sendEmbed(embed);
